ebe8f2cd43e63decaa9471558944680d27b3b175
João Danielewicz <joao.wicz@gmail.com>
2024-11-01 19:40:20 -0300

    commit

--- Atividades/EFDatabaseFirst/EFDatabaseFirst/EFDatabaseFirst/Program.cs
+++ Atividades/EFDatabaseFirst/EFDatabaseFirst/EFDatabaseFirst/Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
 
 
 var connString =
@@ -17,6 +16,9 @@ builder.Services.AddDbContext<MeuBlogContext>(
     o => o.UseSqlServer(connString)
 );
 
+
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
